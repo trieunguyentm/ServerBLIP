01775c78e7045c1c76f361dd4bf4de7984959c50
Ahsen Khaliq <ahsen@huggingface.co>
2022-02-22 04:01:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ description = "Gradio demo for BLIP: Bootstrapping Language-Image Pre-training f
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2201.12086' target='_blank'>BLIP: Bootstrapping Language-Image Pre-training for Unified Vision-Language Understanding and Generation</a> | <a href='https://github.com/salesforce/BLIP' target='_blank'>Github Repo</a></p>"
 
 
-gr.Interface(inference, inputs, outputs, title=title, description=description, article=article, examples=[['starry.jpg',"Image Captioning","None","Nucleus sampling"]]).launch(enable_queue=True)
+gr.Interface(inference, inputs, outputs, title=title, description=description, article=article, examples=[['starrynight.jpeg',"Image Captioning","None","Nucleus sampling"]]).launch(enable_queue=True)
